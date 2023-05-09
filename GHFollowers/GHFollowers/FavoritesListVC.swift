b56d4dfcec1160ba0f8c2c4215cef338f14af789
Serdar Altındaş <serdaraltindas03@gmail.com>
2023-05-09 17:08:18 +0300

    TAB BAR VC

---
+++ GHFollowers/GHFollowers/FavoritesListVC.swift
@@ -0,0 +1,19 @@
+//
+//  FavoritesListVC.swift
+//  GHFollowers
+//
+//  Created by Serdar Altındaş on 9.05.2023.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+    }
+
+}
---
+++ GHFollowers/GHFollowers/SearchVC.swift
@@ -0,0 +1,19 @@
+//
+//  SearchVC.swift
+//  GHFollowers
+//
+//  Created by Serdar Altındaş on 9.05.2023.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
