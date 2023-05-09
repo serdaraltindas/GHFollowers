fc4cda0efba588db2f33a2c40fc2817bb789bc58
Serdar Altındaş <serdaraltindas03@gmail.com>
2023-05-09 17:38:39 +0300

    tabbar

--- GHFollowers/GHFollowers/FavoritesListVC.swift
+++ GHFollowers/GHFollowers/FavoritesListVC.swift
@@ -1,9 +1,3 @@
-//
-//  FavoritesListVC.swift
-//  GHFollowers
-//
-//  Created by Serdar Altındaş on 9.05.2023.
-//
 
 import UIKit
 
@@ -11,7 +5,7 @@ class FavoritesListVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.backgroundColor = .systemRed
         
         
     }
--- GHFollowers/GHFollowers/SearchVC.swift
+++ GHFollowers/GHFollowers/SearchVC.swift
@@ -1,9 +1,3 @@
-//
-//  SearchVC.swift
-//  GHFollowers
-//
-//  Created by Serdar Altındaş on 9.05.2023.
-//
 
 import UIKit
 
@@ -11,7 +5,7 @@ class SearchVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.backgroundColor = .systemBlue
         
     }
 
