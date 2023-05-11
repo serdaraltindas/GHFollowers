9aa6fb80449ef1ddf1780de84e0e93f3ea658f77
Serdar Altındaş <serdaraltindas03@gmail.com>
2023-05-11 19:17:20 +0300

    Fixed

---
+++ GHFollowers/GHFollowers/GFTitleLabel.swift
@@ -0,0 +1,20 @@
+
+
+import UIKit
+
+class GFTitleLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(textAlignment: NSTextAlignment, fontSize: CGFloat) {
+        self.textAlignment = textAlignment
+    
+    }
+
+}
--- GHFollowers/GHFollowers/SearchVC.swift
+++ GHFollowers/GHFollowers/SearchVC.swift
@@ -26,7 +26,7 @@ class SearchVC: UIViewController {
     func createDismissKeyboardTapGesture() {
         let tap = UITapGestureRecognizer(target: self.view , action: #selector(UIView.endEditing))
         view.addGestureRecognizer(tap)
-    }
+    }n
     
     @objc func pushFollowerListVC() {
         guard isUsernameEntered else {
