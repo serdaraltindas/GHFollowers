b3dceeb0d42c44f9bd450e44195b3681d369e3ee
Serdar Altındaş <serdaraltindas03@gmail.com>
2023-05-11 11:13:52 +0300

    custom textField

--- GHFollowers/GHFollowers/SearchVC.swift
+++ GHFollowers/GHFollowers/SearchVC.swift
@@ -2,12 +2,26 @@
 import UIKit
 
 class SearchVC: UIViewController {
+    
+    let logoImageView = UIImageView()
+    let usernameTextField = GFTextField()
+    let callToActionButton = GFButton(backgroundColor: .systemGreen, title: "Get Followers")
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBlue
+        view.backgroundColor = .systemBackground
         
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
 
+    func configureLogoImageView() {
+        view.addSubview(logoImageView)
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        logoImageView.image = UIImage(named: "gh-logo")
+    }
 
 }
